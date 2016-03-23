bd7e5f4096624e53656534bb2904929222b3695f
Ryan Moylan <contact@ryanmoylan.com>
2016-03-23 00:14:48 -0400

    Added Redux Tools - Copied from mern

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map', 
+  devtool: 'cheap-module-eval-source-map',
 
   entry: ['webpack-hot-middleware/client',
           './client/index.js'
@@ -32,6 +32,11 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        CLIENT: JSON.stringify(true)
+      }
+    })
   ]
 }
